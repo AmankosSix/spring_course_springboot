95b68035e6ac7ba19d2db63cde97ef1291757d3e
Amankos Tleukulov <amankostleukulov@gmail.com>
2023-12-27 18:36:07 +0600

    Refactor to use jpa in dao

--- src/main/java/com/amankos/sprig/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
+++ src/main/java/com/amankos/sprig/springboot/spring_course_springboot/dao/EmployeeDAOImpl.java
@@ -2,10 +2,9 @@ package com.amankos.sprig.springboot.spring_course_springboot.dao;
 
 import com.amankos.sprig.springboot.spring_course_springboot.entity.Employee;
 import jakarta.persistence.EntityManager;
-import org.hibernate.Session;
+import jakarta.persistence.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -17,28 +16,26 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getEmployees() {
-        Session session = entityManager.unwrap(Session.class);
-
-
-        return session.createQuery("from Employee", Employee.class).getResultList();
+        Query query = entityManager.createQuery("from Employee");
+        List<Employee> employees = query.getResultList();
+        return employees;
     }
 
     @Override
     public void saveEmployee(Employee employee) {
-        Session session = entityManager.unwrap(Session.class);
-        session.merge(employee);
+        Employee newEmp = entityManager.merge(employee);
+        employee.setId(newEmp.getId());
     }
 
     @Override
     public Employee getEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        return session.get(Employee.class, id);
+        Employee employee = entityManager.find(Employee.class, id);
+        return employee;
     }
 
     @Override
     public void deleteEmployee(int id) {
-        Session session = entityManager.unwrap(Session.class);
-        Query query = session.createQuery("delete from Employee where id=:id");
+        Query query = entityManager.createQuery("delete from Employee where id=:id");
         query.setParameter("id", id);
         query.executeUpdate();
     }
